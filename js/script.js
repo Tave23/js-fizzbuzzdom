6885250725e8ebd9d12c5d26c33b25b1f2031f8d
Davide Tavella <tavella01@gmail.com>
2021-10-25 15:53:52 +0200

    cambio colore boc

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const row = document.querySelector('.row');
+
+for (let i = 1; i <= 100; i++) {
+   const box = document.createElement('div');
+   box.className = 'box';
+   box.innerHTML = i;
+   row.append(box);
+   console.log(box);
+
+   if (!(i % 3) && !(i % 5)) {
+      box.classList.add('mult3-5');
+   } else if (!(i % 3)) {
+      box.classList.add('mult3');
+   } else if (!(i % 5)) {
+      box.classList.add('mult5');
+   } 
+
+   console.log(i);
+}
+
